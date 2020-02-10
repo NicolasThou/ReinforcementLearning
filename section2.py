e02838c93f72b1d04d89f51eeaa274d332a7048a
Nicolas THOU <46757633+NicolasThou@users.noreply.github.com>
2020-02-10 20:58:09 +0100

    Add files via upload

---
+++ section2.py
@@ -0,0 +1,154 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+
+"""
+Implement the different components of the domain. Implement a rule-based policy of
+your choice (e.g., always go left, select actions always at random...). Simulate the
+policy in the domain and display the trajectory.
+"""
+
+def initialize_domain(n,m):
+    """
+    Initialize a domain with n row and m column, for each cells there is a reward value.
+    The reward value for each cells are random.
+
+    Argument:
+    =========
+    n number of row
+    m number of column
+
+    Return:
+    =======
+    return a list
+    """
+    domain = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(random.randint(-6, 10))
+        domain.append(row)
+    return np.array(domain)
+
+
+def action_space():
+    """
+    Initialize the action space
+
+    Return:
+    ======
+    return a list with the 4 movement authorize
+    """
+    return [[1,0], [-1,0], [0,1], [0,-1]]  # down, up, right, left
+
+
+domain = initialize_domain(5,6)  # domain use for the all assignement
+a = action_space()  # a is the action space
+
+
+def policy_left(x):
+    """
+    return the action to do according to a state. It always turning left.
+
+    Argument:
+    ========
+    x is a list of the coordinate
+
+    Return:
+    =======
+    return a list in action space which refers to an action (here is left)
+    """
+
+    return a[3]
+
+def policy_random(x):
+    """
+    return the action to do according to a state. It choose a random action
+
+    Argument:
+    ========
+    x is a list of the coordinate
+
+    Return:
+    =======
+    return a list in action space which refers to a random action
+    """
+    n = random.randint(0,len(a)-1)
+    return a[n]
+
+def next_state(domain, state, action):
+    """
+    Compute the next state
+
+    Argument:
+    ========
+    domain : is the domain
+    state : is the state where we from
+    action : is the action we take to move
+
+    return:
+    ======
+    return a list, which represent the new state after the move
+    """
+
+    size = np.shape(domain)
+    n, m = size[0], size[1]
+    x = min(max(state[0]+action[0], 0), n-1)
+    y = min(max(state[1]+action[1], 0), m-1)
+    new_state = [x,y]
+    return new_state
+
+def simulation(domain, initial_state, step):
+    """
+    Simulate the policy left in the domain and display the trajectory.
+
+    Argument:
+    ========
+    domain : is the domain
+    initial_state : is the state where we begin, it is a list of 2 integers [x, y]
+    step : is the number of step the agent make a move
+
+    Return:
+    ======
+    return the historic, and the trajectory. Here the historic is like (x(0), u(0), r(0), ..., u(t-1), r(t-1), x(t))
+    and the trajectory is like (x(0), x(1), x(2), ..., x(t-1), x(t)). So in the historic we have the reward and
+    the action also.
+    """
+
+    historic = [initial_state]
+    trajectory = [initial_state]
+    m = []
+    state = initial_state
+
+    for i in range(step):
+        action = policy_random(state)  # here we use the random policy but we can change it by the left policy
+        new_state = next_state(domain, state, action)
+        reward = domain[new_state[0], new_state[1]]
+        historic.append(action)
+        historic.append(reward)
+        historic.append(new_state)
+        trajectory.append(new_state)
+        state = new_state
+
+    return historic, trajectory
+
+
+
+if __name__ == '__main__':
+
+    #  test of the initialize_domain, and the 2 policies
+
+    domain = initialize_domain(6,5)
+    print(domain)
+    a1 = policy_left([2,3])
+    print(a1)  # always [1,0]
+    a2 = policy_random([2,3])
+    print(a2)  # random action
+
+    #  test the simulation
+
+    historic, trajectory = simulation(domain, [2,4], 7)
+    print(trajectory)
+
+
+
---
+++ section3.py
@@ -0,0 +1,67 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+from section2 import *
+
+"""
+
+Implement a routine to estimate J^µ in this domain, where µ : X → U is a stationary
+policy. The implementation should exploit the dynamic programming principle where
+a sequence of functions J_N for N = 0, 1, 2... is computed using the Bellman Equation.
+Test your implementation with your rule-based policy of Section 2. Choose a N which
+is large enough to approximate well the infinite time horizon of the policy and motivate
+your choice. Display J_N_µ(x) for each state x.
+
+"""
+
+
+def value_function(x, n):
+    """
+    Compute the return of a stationary policy thanks to the Bellman Equation
+    We use the policy_left to compute each J(x)
+    The discount factor gamma is equal to 0.99
+
+    Argument:
+    ========
+    x : is the initial state
+    n : number of value function
+
+    Return:
+    =======
+    return an integer, which is the estimation of J^µ
+
+    """
+    if n == 0:
+        return 0
+    else:
+        action = policy_left(x)  # here we use the policy
+        new_state = next_state(domain, x, action)
+        reward = domain[new_state[0], new_state[1]]
+        return reward + 0.99 * value_function(new_state, n-1)
+
+
+def display(step):
+    """
+    Display J_N_µ(x) for each state x
+    """
+    print(domain)
+    x = [0, 0]
+    n, m = np.shape(domain)
+    tableau = []  # table of J_N_µ(x) for each state x
+    for i in range(n):
+        row = []
+        for j in range(m):
+            x[0] = i
+            x[1] = j
+            row.append(value_function(x, step))
+        tableau.append(row)
+
+    tableau = np.array(tableau)
+    print(tableau)
+
+
+if __name__ == '__main__':
+    display(500)  # we can choose a N which is large enough to approximate well the infinite time horizon of the policy
+
+
+
