07262d8f1795806ff0f0c9b738fb18a2b4f76f9c
BilGuet <b.guetarni@laposte.net>
2020-02-10 22:45:48 +0100

    Add visualization

--- section2.py
+++ section2.py
@@ -1,12 +1,26 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import random
+import time
 
 """
 Implement the different components of the domain. Implement a rule-based policy of
 your choice (e.g., always go left, select actions always at random...). Simulate the
 policy in the domain and display the trajectory.
 """
+def draw(n, m, domain, position):
+    # display the environment
+    fig, ax = plt.subplots(figsize=(4, 4))
+    ax.axis('off')
+    cell_text = np.asarray(domain, dtype=np.str)
+    colors = [["w" for i in range(m)] for j in range(n)]
+    # colors = [["w", "w", "w", "w", "w"], ["w", "w", "w", "w", "w"], ["w", "w", "w", "w", "w"],
+    # ["w", "w", "w", "w", "w"], ["w", "w", "w", "w", "w"]]
+    colors[position[0]][position[1]] = "r"
+    ax.table(cellText=cell_text, cellColours=colors, cellLoc='center', loc='center',
+             colWidths=[0.07 for i in range(m)])
+    plt.show()
+
 
 def initialize_domain(n,m):
     """
@@ -119,7 +133,8 @@ def simulation(domain, initial_state, step):
     trajectory = [initial_state]
     m = []
     state = initial_state
-
+    draw(6, 5, domain, initial_state)
+    time.sleep(2)
     for i in range(step):
         action = policy_random(state)  # here we use the random policy but we can change it by the left policy
         new_state = next_state(domain, state, action)
@@ -129,6 +144,9 @@ def simulation(domain, initial_state, step):
         historic.append(new_state)
         trajectory.append(new_state)
         state = new_state
+        print(state)
+        draw(6, 5, domain, new_state)
+        time.sleep(2)
 
     return historic, trajectory
 
